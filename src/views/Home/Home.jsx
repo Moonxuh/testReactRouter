311455f730789d1835849fc5a2f5a2958568a771
Lara <laralansone64@gmail.com>
2025-02-18 10:17:13 -0300

    Use and Mixin test

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './index.scss'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <button><Link to={`/products/1`}>Produto</Link></button>
+    <div className='home'>
+      <div className="container">
+        <h1>
+          Home<span>!</span>
+        </h1>
+      </div>
+  
+      <button>
+        <Link to={`/products/1`}>Produto</Link>
+      </button>
+
+      <button>
+        Testando Main-Button
+      </button>
     </div>
   )
 }
